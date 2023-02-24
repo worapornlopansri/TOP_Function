5a593ba2c3021b6525e8795880cc53b0f9959260
X505Z <dev@local.email>
2023-02-24 17:41:42 +0700

    Commit 1

--- functions/myfunction/index.js
+++ functions/myfunction/index.js
@@ -13,7 +13,7 @@ export default async function (event, context, logger) {
   const account = {
     type: "Account",
     fields: {
-      Name: `${name}-${Date.now()}`,
+      Name: `${name}-${Date.now()}-MODIFIED`,
       AccountNumber: accountNumber,
       Industry: industry,
       Type: type,
