a6c6bd0a64c1de935eae9dba811327537894edd1
woraporn <woraporn.l@roundtwosolutions.com>
2023-02-24 16:43:57 +0700

    test commit

---
+++ functions/myfunction/index.js
@@ -0,0 +1,38 @@
+export default async function (event, context, logger) {
+  logger.info(`Invoking salesforcesdkjs function with payload ${JSON.stringify(event.data || {})}`);
+
+  // Extract properties from payload
+  const { name, accountNumber, industry, type, website } = event.data;
+
+  // Validate the payload params
+  if (!name) {
+    throw new Error(`Please provide account name`);
+  }
+
+  // Define a record using the RecordForCreate type and providing the Developer Name
+  const account = {
+    type: "Account",
+    fields: {
+      Name: `${name}-${Date.now()}`,
+      AccountNumber: accountNumber,
+      Industry: industry,
+      Type: type,
+      Website: website,
+    },
+  };
+
+  try {
+    // Insert the record using the SalesforceSDK DataApi and get the new Record Id from the result
+    const { id: recordId } = await context.org.dataApi.create(account);
+
+    // Query Accounts using the SalesforceSDK DataApi to verify that your new Account was created.
+    const soql = `SELECT Fields(STANDARD) FROM Account WHERE Id = '${recordId}'`;
+    const queryResults = await context.org.dataApi.query(soql);
+    return queryResults;
+  } catch (err) {
+    // Catch any DML errors and pass the throw an error with the message
+    const errorMessage = `Failed to insert record. Root Cause: ${err.message}`;
+    logger.error(errorMessage);
+    throw new Error(errorMessage);
+  }
+}
