aeee46cf6298d1a6e5b8c1ea4999bd8886635b1b
An <an.tham.41@gmail.com>
2023-04-05 12:59:29 +0700

    Initial project commit after project creation

--- functions/AccFunction/index.js
+++ functions/AccFunction/index.js
@@ -13,22 +13,16 @@ export default async function (event, context, logger) {
   const account = {
     type: "Account",
     fields: {
+      id : id,
       Name: `${name}-${Date.now()}-Modified123`,
-      AccountNumber: accountNumber,
-      Industry: industry,
-      Type: type,
-      Website: website,
+      AccountNumber: 'test'
     },
   };
 
   try {
     // Insert the record using the SalesforceSDK DataApi and get the new Record Id from the result
-    const { id: recordId } = await context.org.dataApi.create(account);
+    const UpdateAccount = await context.org.dataApi.update(account);
 
-    // Query Accounts using the SalesforceSDK DataApi to verify that your new Account was created.
-    const soql = `SELECT Fields(STANDARD) FROM Account WHERE Id = '${recordId}'`;
-    const queryResults = await context.org.dataApi.query(soql);
-    //return queryResults;
     return myData();
   } catch (err) {
     // Catch any DML errors and pass the throw an error with the message
