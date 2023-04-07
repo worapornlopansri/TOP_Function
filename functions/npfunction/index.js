066288a130d79002568e24183a57f63cc12f263f
NP_FON <you@example.com>
2023-04-07 12:56:21 +0700

    TT

--- functions/npfunction/index.js
+++ functions/npfunction/index.js
@@ -14,7 +14,7 @@ export default async function (event, context, logger) {
     type: "Account",
     fields: {
       Name: `${name}-${Date.now()}-Modified123`,
-      AccountNumber: accountNumber,
+      AccountNumber: `${accountNumber}`,
       Industry: industry,
       Type: type,
       Website: website,
