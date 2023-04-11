697dfa3f9f408db7d6b3c6d2393baa57897bbeee
NP_FON <you@example.com>
2023-04-11 07:27:00 +0700

    ttt

--- functions/npmyfunction/index.js
+++ functions/npmyfunction/index.js
@@ -1,34 +1,36 @@
 export default async function (event, context, logger) {
   logger.info(`Invoking salesforcesdkjs function with payload ${JSON.stringify(event.data || {})}`);
-  //test test 4/3/2023
+
   // Extract properties from payload
-  const { id, name, Phone } = event.data;
-//test may
+  const { name, phone } = event.data;
+
   // Validate the payload params
   if (!name) {
-    throw new Error(`Please provide account name345`);
+    throw new Error(`Please provide account name`);
   }
 
-  // Define a record using the RecordFoCreate type and providing the Developer Name
+  // Define a record using the RecordForCreate type and providing the Developer Name
   const account = {
     type: "Account",
     fields: {
-      id : id,
       Name: `${name}-${Date.now()}`,
-      Phone: Phone
+      //AccountNumber: accountNumber,
+      phone: phone,
     },
   };
 
   try {
     // Insert the record using the SalesforceSDK DataApi and get the new Record Id from the result
-    const UpdateAccount = await context.org.dataApi.update(account);
+    const { id: recordId } = await context.org.dataApi.update(account);
 
-    return UpdateAccount;
+    // Query Accounts using the SalesforceSDK DataApi to verify that your new Account was created.
+    const soql = `SELECT Fields(STANDARD) FROM Account WHERE Id = '${recordId}'`;
+    const queryResults = await context.org.dataApi.query(soql);
+    return queryResults;
   } catch (err) {
     // Catch any DML errors and pass the throw an error with the message
     const errorMessage = `Failed to insert record. Root Cause: ${err.message}`;
     logger.error(errorMessage);
     throw new Error(errorMessage);
-    return errorMessage;
   }
 }
