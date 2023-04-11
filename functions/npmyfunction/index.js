0ebd9d37e37f8b741c763b586681a319d021a850
NP_FON <you@example.com>
2023-04-11 07:03:24 +0700

    Function

--- functions/npmyfunction/index.js
+++ functions/npmyfunction/index.js
@@ -1,21 +1,34 @@
-/**
- * Describe Npmyfunction here.
- *
- * The exported method is the entry point for your code when the function is invoked.
- *
- * Following parameters are pre-configured and provided to your function on execution:
- * @param event: represents the data associated with the occurrence of an event, and
- *                 supporting metadata about the source of that occurrence.
- * @param context: represents the connection to Functions and your Salesforce org.
- * @param logger: logging handler used to capture application logs and trace specifically
- *                 to a given execution of a function.
- */
 export default async function (event, context, logger) {
-  logger.info(`Invoking Npmyfunction with payload ${JSON.stringify(event.data || {})}`);
+  logger.info(`Invoking salesforcesdkjs function with payload ${JSON.stringify(event.data || {})}`);
+  //test test 4/3/2023
+  // Extract properties from payload
+  const { id,name, accountNumber, industry, type, website } = event.data;
+//test may
+  // Validate the payload params
+  if (!name) {
+    throw new Error(`Please provide account name345`);
+  }
 
-  const results = await context.org.dataApi.query('SELECT Id, Name FROM Account');
+  // Define a record using the RecordFoCreate type and providing the Developer Name
+  const account = {
+    type: "Account",
+    fields: {
+      id : id,
+      Name: `${name}-${Date.now()}`,
+      Phone: Phone
+    },
+  };
 
-  logger.info(JSON.stringify(results));
+  try {
+    // Insert the record using the SalesforceSDK DataApi and get the new Record Id from the result
+    const UpdateAccount = await context.org.dataApi.update(account);
 
-  return results;
+    return UpdateAccount;
+  } catch (err) {
+    // Catch any DML errors and pass the throw an error with the message
+    const errorMessage = `Failed to insert record. Root Cause: ${err.message}`;
+    logger.error(errorMessage);
+    throw new Error(errorMessage);
+    return errorMessage;
+  }
 }
