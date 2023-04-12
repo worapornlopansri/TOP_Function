79ae309fb3cd9d9b58b58e7466a1a1a48ae7517a
chuntikab <chuntika.bum@gmail.com>
2023-04-12 14:17:52 +0700

    commit 2 function for  inspec report

---
+++ functions/inspecreport/src/main/java/com/example/MainWorkCenter.java
@@ -0,0 +1,50 @@
+package com.example;
+
+public class MainWorkCenter {
+    private String opsmwc;
+    private String crdYear;
+    private String crdMonth;
+    private String crtdt;
+    private String sum;
+
+    public String getOpsmwc() {
+        return this.opsmwc;
+    }
+
+    public void setName(String opsmwc) {
+        this.opsmwc = opsmwc;
+    }   
+
+    public String getCrdYear() {
+        return this.crdYear;
+    }
+
+    public void setCrdYear(String crdYear) {
+        this.crdYear = crdYear;
+    }    
+    
+    public String getCrdMonth() {
+        return this.crdMonth;
+    }
+
+    public void setCrdMonth(String crdMonth) {
+        this.crdMonth = crdMonth;
+    }    
+    
+    public String getCrtdt() {
+        return this.crtdt;
+    }
+
+    public void setCrtdt(String crtdt) {
+        this.crtdt = crtdt;
+    }    
+    
+    public String getSum() {
+        return this.sum;
+    }
+
+    public void setSum(String sum) {
+        this.sum = sum;
+    }
+    
+}
