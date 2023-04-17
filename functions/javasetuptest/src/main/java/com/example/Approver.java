25599cc12763faa555d6d2b9c83949b191c17686
woraporn <woraporn.l@roundtwosolutions.com>
2023-04-17 16:26:40 +0700

    java

--- functions/javasetuptest/src/main/java/com/example/Approver.java
+++ functions/javasetuptest/src/main/java/com/example/Approver.java
@@ -1,31 +1,32 @@
 package com.example;
 
 public class Approver {
-    private final String developerName;
-    private final String requesterManager;
-    private final String requesterVP;
-    private final String DGVP;
+    private final String DeveloperName;
+    private final String Requester_Manager__c;
+    private final String Requester_VP__c;
+    private final String DGVP__c;
 
     public Approver(String developerName, String requesterManager, String requesterVP, String DGVP) {
-        this.developerName = developerName;
-        this.requesterManager = requesterManager;
-        this.requesterVP = requesterVP;
-        this.DGVP = DGVP;
+        this.DeveloperName = developerName;
+        this.Requester_Manager__c = requesterManager;
+        this.Requester_VP__c = requesterVP;
+        this.DGVP__c = DGVP;
     }
 
     public String getDeveloperName() {
-        return this.developerName;
+        return this.DeveloperName;
     }
 
-    public String getRequesterManager() {
-        return this.requesterManager;
+    public String getRequester_Manager__c() {
+        return this.Requester_Manager__c;
     }
 
-    public String getRequesterVP() {
-        return this.requesterVP;
+    public String getRequester_VP__c() {
+        return this.Requester_VP__c;
     }
 
-    public String getDGVP() {
-        return this.DGVP;
+    public String getDGVP__c() {
+        return this.DGVP__c;
     }
+
 }
